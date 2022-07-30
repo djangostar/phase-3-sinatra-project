34a8656c70facf411b29a2184d325bb0ff10e9c9
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2022-07-30 14:10:57 -0700

    Get/post for platforms done

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,4 +4,7 @@ class GamesController < ApplicationController
         Game.all.to_json({include: :platform})
     end
 
+    post '/games' do
+        Game.create(title: params[:title], genre: params[:genre], review: params[:review], year_relased: params[:year_relased], currently_playing: params[:currently_playing], platform_id: params[:platform_id]).to_json
+    end
 end
--- app/controllers/platforms_controller.rb
+++ app/controllers/platforms_controller.rb
@@ -2,4 +2,8 @@ class PlatformsController < ApplicationController
     get '/platforms' do
         Platform.all.to_json
     end
+
+    post '/platforms' do
+        Platform.create(platform_name: params[:platform_name]).to_json
+    end
 end
