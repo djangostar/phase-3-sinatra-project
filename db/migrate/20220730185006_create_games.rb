ad08838496ac5b017078caa0583aac768b52e238
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2022-07-30 12:25:04 -0700

    tables migrated

--- db/migrate/20220730185006_create_games.rb
+++ db/migrate/20220730185006_create_games.rb
@@ -1,4 +1,13 @@
 class CreateGames < ActiveRecord::Migration[6.1]
   def change
+    create_table :games do |t|
+      t.string :title
+      t.string :genre
+      t.text :review
+      t.integer :year_relased
+      t.boolean :currently_playing
+      t.integer :platform_id
+      t.timestamps
+    end
   end
 end
--- db/migrate/20220730185021_create_platoforms.rb
+++ db/migrate/20220730185021_create_platoforms.rb
@@ -1,4 +1,8 @@
 class CreatePlatoforms < ActiveRecord::Migration[6.1]
   def change
+    create_table :platforms do |t|
+      t.string :platform_name
+      t.timestamps
+    end
   end
 end
