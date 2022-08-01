584210fe52d5d04a83d77cbd4763f0fc2ceb43c5
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2022-08-01 00:59:31 -0700

    Got Patch working

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,11 +4,14 @@ class GamesController < ApplicationController
         Game.all.to_json({include: :platform})
     end
 
+    # Add a new game
     post '/games' do
-        Game.create(title: params[:title], genre: params[:genre], review: params[:review], year_relased: params[:year_relased], currently_playing: params[:currently_playing], platform_id: params[:platform_id]).to_json({include: :platform})
+        Game.create(title: params[:title], genre: params[:genre], review: params[:review], year_released: params[:year_released], currently_playing: params[:currently_playing], platform_id: params[:platform_id]).to_json({include: :platform})
     end
 
-    patch '/games/id' do
-        Game.update(title: params[:title], genre: params[:genre], review: params[:review], year_relased: params[:year_relased], currently_playing: params[:currently_playing], platform_id: params[:platform_id]).to_json({include: :platform})
+    # Change active status
+    patch '/games/:id' do
+        game = Game.find(params[:id])
+        game.update(title: params[:title], genre: params[:genre], review: params[:review], year_released: params[:year_released], currently_playing: params[:currently_playing], platform_id: params[:platform_id]).to_json({include: :platform})
     end
 end
